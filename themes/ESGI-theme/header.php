8975cf48f53663917393b30a5c7bbe84f241e81b
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-06-16 19:32:27 +0200

    apply side gaps for the dropdown menu
    Signed-off-by: Dilan EESHVARAN <ieeshvarand@gmail.com>

--- themes/ESGI-theme/header.php
+++ themes/ESGI-theme/header.php
@@ -30,45 +30,50 @@
                     </div>
                 </button>
             </div>
-        </div>
-          <!-- menu dropdown -->
+        </div>          <!-- menu dropdown -->
         <div class="mobile-navigation">
-            <button class="mobile-menu-close" aria-label="Close menu">
-                <?php echo esgi_getIcon('close'); ?>
-            </button>
             <div class="mobile-nav-content">
-                <div class="mobile-logo">
-                    <?php 
-                    if (has_custom_logo()) {
-                        the_custom_logo();
-                    } else {
-                        echo '<a href="' . esc_url(home_url('/')) . '" class="site-logo">';
-                        echo '<img src="' . get_template_directory_uri() . '/src/images/svg/logo.svg" alt="' . get_bloginfo('name') . '" />';
-                        echo '</a>';
-                    }
-                    ?>
+                <div class="container">
+                    <div class="mobile-nav-header">
+                        <div class="mobile-logo">
+                            <?php 
+                            if (has_custom_logo()) {
+                                the_custom_logo();
+                            } else {
+                                echo '<a href="' . esc_url(home_url('/')) . '" class="site-logo">';
+                                echo '<img src="' . get_template_directory_uri() . '/src/images/svg/logo.svg" alt="' . get_bloginfo('name') . '" />';
+                                echo '</a>';
+                            }
+                            ?>
+                        </div>
+                        <button class="mobile-menu-close" aria-label="Close menu">
+                            <?php echo esgi_getIcon('close'); ?>
+                        </button>
+                    </div>
                 </div>
-                <nav class="mobile-nav-menu">
-                    <?php
-                    if (has_nav_menu('primary_menu')) {
-                        wp_nav_menu([
-                            'container' => false,
-                            'theme_location' => 'primary_menu',
-                            'menu_class' => 'mobile-menu-list'
-                        ]);
-                    }/*  else {
-                        // fallback menu if no menu is set
-                        echo '<ul class="mobile-menu-list">';
-                        echo '<li><a href="' . esc_url(home_url('/')) . '">Home</a></li>';
-                        echo '<li><a href="' . esc_url(home_url('/about-us')) . '">About Us</a></li>';
-                        echo '<li><a href="' . esc_url(home_url('/services')) . '">Services</a></li>';
-                        echo '<li><a href="' . esc_url(home_url('/partners')) . '">Partners</a></li>';
-                        echo '<li><a href="' . esc_url(home_url('/blog')) . '">Blog</a></li>';
-                        echo '<li><a href="' . esc_url(home_url('/contacts')) . '">Contacts</a></li>';
-                        echo '</ul>';
-                    } */
-                    ?>
-                </nav>
+                <div class="container">
+                    <nav class="mobile-nav-menu">
+                        <?php
+                        if (has_nav_menu('primary_menu')) {
+                            wp_nav_menu([
+                                'container' => false,
+                                'theme_location' => 'primary_menu',
+                                'menu_class' => 'mobile-menu-list'
+                            ]);
+                        }/*  else {
+                            // fallback menu if no menu is set
+                            echo '<ul class="mobile-menu-list">';
+                            echo '<li><a href="' . esc_url(home_url('/')) . '">Home</a></li>';
+                            echo '<li><a href="' . esc_url(home_url('/about-us')) . '">About Us</a></li>';
+                            echo '<li><a href="' . esc_url(home_url('/services')) . '">Services</a></li>';
+                            echo '<li><a href="' . esc_url(home_url('/partners')) . '">Partners</a></li>';
+                            echo '<li><a href="' . esc_url(home_url('/blog')) . '">Blog</a></li>';
+                            echo '<li><a href="' . esc_url(home_url('/contacts')) . '">Contacts</a></li>';
+                            echo '</ul>';
+                        } */
+                        ?>
+                    </nav>
+                </div>
             </div>
         </div>
     </header>
