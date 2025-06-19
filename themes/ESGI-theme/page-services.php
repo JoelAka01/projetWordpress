29a989ddbdcc71486711e9baaf227c21b411d52a
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-06-20 00:14:05 +0200

    fix duplicate footer in services page
    Signed-off-by: Dilan EESHVARAN <ieeshvarand@gmail.com>

--- themes/ESGI-theme/page-services.php
+++ themes/ESGI-theme/page-services.php
@@ -14,7 +14,7 @@ get_header();
                  <div class="container">       
             <div class="row">
                 <div class="col-12">
-                    <h2 class="services-title"><?php echo get_theme_mod('services_title', 'Our Services'); ?></h2>
+                    <h2 class="services-title"><?php echo get_theme_mod('services_title', 'Our Services.'); ?></h2>
                 </div>
             </div>
             </div>            
