7eb1e32b2d323953f656ed059d040a972c2d723c
Aka <94462079+JoelAka01@users.noreply.github.com>
2025-07-14 09:22:16 +0200

    contact page creation

--- themes/ESGI-theme/page-contacts.php
+++ themes/ESGI-theme/page-contacts.php
@@ -19,24 +19,69 @@ get_header();
             </div>
         </div>
 
-        <div class="contacts-info">
-            <div class="row">
-                <div class="col-md-6">
+        <div class="contacts-info text-end">
+            <div class="row d-flex flex-row-reverse ">
+                <div class="col-md-3">
                     <div class="contact-item">
                         <h3><?php echo get_theme_mod('footer_contact_title', 'Manager'); ?></h3>
-                        <p><strong>Phone:</strong> <?php echo get_theme_mod('footer_contact_phone', '+33 1 53 31 25 23'); ?></p>
-                        <p><strong>Email:</strong> <?php echo get_theme_mod('footer_contact_email', 'info@esgi.com'); ?></p>
+                        <p><?php echo get_theme_mod('footer_contact_phone', '+33 1 53 31 25 23'); ?></p>
+                        <p><?php echo get_theme_mod('footer_contact_email', 'info@esgi.com'); ?></p>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <div class="contact-item">
                         <h3><?php echo get_theme_mod('footer_ceo_title', 'CEO'); ?></h3>
-                        <p><strong>Phone:</strong> <?php echo get_theme_mod('footer_ceo_phone', '+33 1 53 31 25 25'); ?></p>
-                        <p><strong>Email:</strong> <?php echo get_theme_mod('footer_ceo_email', 'ceo@company.com'); ?></p>
+                        <p><?php echo get_theme_mod('footer_ceo_phone', '+33 1 53 31 25 25'); ?></p>
+                        <p><?php echo get_theme_mod('footer_ceo_email', 'ceo@company.com'); ?></p>
+                    </div>
+                </div>
+                    <div class="col-md-3">
+                    <div class="contact-item">
+                        <h3><?php echo get_theme_mod('footer_location_title', 'location'); ?></h3>
+                        <p><?php echo get_theme_mod('footer_location_address', '242 Rue du Faubourg Saint-Antoine 75020 Paris FRANCE'); ?></p>
                     </div>
                 </div>
             </div>
         </div>
+                <div class="row d-flex flex-row-reverse">
+            <div class="col-6">
+                <div class="hero-image-wrapper">
+                    <div class="hero-image">
+                        <?php
+                        $hero_image = get_theme_mod('hero_image');
+                        if ($hero_image) {
+                            echo '<img src="' . esc_url($hero_image) . '" alt="Hero Image" class="img-fluid">';
+                        } else {
+                            // display default hero image from theme
+                            echo '<img src="' . get_template_directory_uri() . '/src/images/png/10.png" alt="Hero Image" class="img-fluid">';
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="wirteus-section">
+        <div class="container">
+            <h2> Write us here</h2>
+            <p>Gol Don't be shy </p>
+
+            <div class="contact-form-contain">
+                <form method="get" class="contact-form row" action="<?php echo esc_url(home_url('/')); ?>">
+                    <input type="text" class="contact-field col-12" placeholder="Subject"  name="name" />
+                    <input type="email" class="contact-field col-5" placeholder="Email" name="email" />
+                    <input type="number" class="contact-field col-5" placeholder="phone" name="phone" />
+                    <textarea class="contact-field col-12" placeholder="Message" name="message"></textarea>
+                    <button type="submit" class="search-submit">Submit </button>
+                </form>
+
+            </div>
+
+
+
+        </div>
+
     </div>
 </main>
 
